d9e183f74f08975f8217b82f435dfc9ee7c998bb
Leonardo Villela <leonardo.villela37@gmail.com>
2017-03-22 23:45:28 -0300

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import SearchBar from './seacher_bar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <SearchBar />
+    );
+  }
+}
